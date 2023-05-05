7ee9d55aa0b165cb9fca99b3415d4c8635325eab
David Prows <david@prowshouse.com>
2023-05-04 20:54:13 -0400

    update create_badge.py and added readme for JBrant

--- create_badge.py
+++ create_badge.py
@@ -9,7 +9,7 @@ parser = argparse.ArgumentParser()
 # Add command-line arguments for name, company, and title
 parser.add_argument("--name", help="Your name", default=" ")
 parser.add_argument("--company", help="Your company", default=" ")
-parser.add_argument("--title", help="Your job title", default="Attendee")
+parser.add_argument("--status", help="Your conference status", default="Attendee")
 
 # Parse the command-line arguments
 args = parser.parse_args()
@@ -17,14 +17,14 @@ args = parser.parse_args()
 # Set other variables
 conference = "BSides Fort Wayne 2023"
 picture = "/badges/BadgeLogo.jpg"
-badge = "/home/user/badge.txt"
-image = "/home/user/BadgeLogo.jpg"
-badge_script = "/home/user/badge.py"
+badge = "/home/user/Documents/Badger_2040/badge.txt"
+image = "/home/user/Documents/Badger_2040/BadgeLogo.jpg"
+badge_script = "/home/user/Documents/Badger_2040/badge.py"
 serial_port = "/dev/ttyACM0"
 
 # Write the information to a file called badge.txt
 with open(badge, "w") as f:
-    f.write(f"{args.title}\n{args.name}\n{args.company}\n\n{conference}\n\n{picture}")
+    f.write(f"{args.status}\n{args.name}\n{args.company}\n\n{conference}\n\n{picture}")
 
 # Wait for the Badger 2040 board to be ready
 time.sleep(2)
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Badger 2040 badge script
+
+This is a work in progress and more information will be added soon.
